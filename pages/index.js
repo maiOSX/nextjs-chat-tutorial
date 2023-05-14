9806e213dee5ce2664f9edf26bd4090f5a56009a
maiOSX <124741903+maiOSX@users.noreply.github.com>
2023-05-14 15:35:10 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,67 @@
-import React, { useState, createContext } from "react";
+import React from "react";
+import React, { useContext } from "react";
 
-export const Context = createContext();
+export default function Auth() {
+  return <div className="background">auth</div>;
+}
+import { Context } from "../context";
 
-export const ContextProvider = (props) => {
-  const [username, setUsername] = useState("");
-  const [secret, setSecret] = useState("");
+import { useRouter } from "next/router";
 
-  const value = {
-    username,
-    setUsername,
-    secret,
-    setSecret,
-  };
+import axios from "axios";
 
-  return <Context.Provider value={value}>{props.children}</Context.Provider>;
+const Auth = () => {
+  const { username, setUsername, secret, setSecret } = useContext(Context);
+
+  const router = useRouter();
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    if (username.length === 1 || secret.length === 1) return;
+
+    axios
+      .put(
+        "https://api.chatengine.io/users/",
+        { username, secret },
+        { headers: { "Private-Key": "c2f82e63-9978-4c5c-9c17-8b0dec845dc6" } }
+      )
+
+      .then((r) => {
+        router.push("/chats");
+      });
+  }
+
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
+          <div className="auth-title">NextJS Chat</div>
+
+          <div className="input-container">
+            <input
+              placeholder="Email"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-input"
+              onChange={(e) => setSecret(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="submit-button">
+            Login / Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
+
+export default Auth;
